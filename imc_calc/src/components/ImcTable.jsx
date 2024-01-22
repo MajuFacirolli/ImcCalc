1b0975b699ea76aec54a63c3d6c0f150a4aee65b
Maju Facirolli <majufacirolli2004@hotmail.com>
2024-01-22 11:21:09 -0300

    Imc Table Structure

--- imc_calc/src/components/ImcTable.jsx
+++ imc_calc/src/components/ImcTable.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import './ImcTable.css'
+import Button from './Button'
 
-const ImcTable = () => {
+const ImcTable = ({data}) => {
   return (
-    <div>ImcTable</div>
+    <div className="result_container">
+      <p className="imc_number">Seu Imc: </p>
+      <p className="imc_info">Situação Atual:</p>
+      <h3>Classificações:</h3>
+      <div className="imc_table">
+        <div className="table_header">
+          <h4>IMC</h4>
+          <h4>Classificação</h4>
+          <h4>Obesidade</h4>
+        </div>
+        {data.map((item) => (
+          <div className="table_data" key={item.info}>
+            <p>{item.classification}</p>
+            <p>{item.info}</p>
+            <p>{item.obesity}</p>
+          </div>
+        ))}
+      </div>
+      <Button id="back_btn" text="Voltar"/>
+    </div>
   )
 }
 
